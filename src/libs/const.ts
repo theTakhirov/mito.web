a70645e6994e13eac07d84ae7f0e185f68359481
Диёр Тохиров <mtohirov60@gmail.com>
2022-03-20 01:28:43 +0500

    add libs `(constants)`

---
+++ src/libs/const.ts
@@ -0,0 +1,13 @@
+export const name = {
+    DEFAULT: 'Muhammaddiyor Tohirov',
+    firstName: 'Muhammaddiyor',
+    lastName: 'Tohirov',
+    nickName: 'mitoGen'
+}
+
+export const social = {
+    telegram: 'mitoGenic',
+    facebook: 'thetakhirov',
+    instagram: 'thetakhirov',
+    github: 'thetakhirov'
+}
---
+++ src/libs/ease.ts
@@ -0,0 +1,3 @@
+export const easeInOut = {
+    ease: [0.25, 0.95, 0.45, 0.70]
+}
